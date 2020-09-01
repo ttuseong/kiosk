894a602e0dacaca189adfc65572abb47aaffd7ee
jm00476 <jm00476@naver.com>
2020-09-01 23:44:28 +0900

    타이틀 중복시 리스트 추가 불가능 하도록 구현 완료

--- webapp/assets/js/admin/adminCate.js
+++ webapp/assets/js/admin/adminCate.js
@@ -1,46 +1,8 @@
 var url = window.location.pathname.substring(0,window.location.pathname.indexOf("/",2));
 
-/*카테고리 x버튼 누르면 삭제하기 메뉴가 있으면 삭제가 되면 안된다.. --삭제기능..*/
-$(".adminCate-delete").on("click", function(){
-	event.preventDefault();
-	console.log("클릭");
-	var cateDelete = $(this);
-	
-	console.log(cateDelete);
-	var categoryNo = cateDelete.parent().parent().data("no"); //data값 no를 가져온 이유는 최상위 값만 삭제하면 그 안에있던 데이터도 같이 삭제되기때문에(ex폴더)
-	console.log($('[data-no=' + categoryNo + ']'));
-	console.log(cateDelete.parent().parent());
-	console.log(categoryNo);
-	/*var categoryNo = $("#");*/
-	
-	$.ajax({
-		url : url+"/admin/adminCateDel",
-		type: "post",
-		data : {categoryNo : categoryNo},
-		dataType : "json",
-		success : function(cnt){
-			console.log(cnt);
-			/*성공시 처리해야 될 코드 작성 */
-			if(cnt==0){
-				console.log("실패");
-				alert('카테고리에 값이 남아있습니다.');
-			}
-			else{
-				console.log("넌 그냥 삭제");
-				$('[data-no=' + categoryNo + ']').remove();
-			}
-		},
-		error : function(XHR, status, error) {
-			console.error(status + " : " + error);
-		}	
-	});
-
-});
-
-
 
 /* 카테고리 추가 및 수정 */
-$("#btnCateNameCheck").on("click", function(){
+$(".adminCate-submitBtn").on("click", ".adminCate-btn",function(){
 	event.preventDefault();
 	console.log("클릭");
 	console.log(url);
@@ -51,34 +13,8 @@ $("#btnCateNameCheck").on("click", function(){
 			cateName:adminCateName
 	};
 	
-	$.ajax({
-		
-		url : url+"/admin/adminCateUpdate",
-		type: "post",
-		data : adminCateInfo,
-		
-		dataType : "json",
-		success : function(result){
-			console.log(result);
-			/*성공시 처리해야 될 코드 작성 */
-			if(result == true) {
-				alert("사용가능합니다")
-			}else {
-				alert("중복된 이름입니다.")
-			}
-		},
-		error : function(XHR, status, error) {
-			console.error(status + " : " + error);
-		}
-		
-	});
-});
 
-/* 확인 버튼 누르면 카테고리 목록에 추가*/
-$(".adminCate-submitBtn").on("click", ".adminCate-btn",function(){
-	event.preventDefault();
-	console.log("확인클릭");
-	
+	//타이틀값, 공개 비공개 값 읽어오기
 	var cateTitle = $(".adminCate-addCateForm").val();
 	console.log(cateTitle);
 	
@@ -90,29 +26,56 @@ $(".adminCate-submitBtn").on("click", ".adminCate-btn",function(){
 			publicYN: adminCatePublicYn
 	};
 	
-	
 	$.ajax({
+		
+		url : url+"/admin/adminCateUpdate",
+		type: "post",
+		data : adminCateInfo,
+		dataType : "json",
+		success : function(result){
+			console.log(result);
+			/*성공시 처리해야 될 코드 작성 */
+			//확인버튼 눌렀을시 타이틀이 중복되었을경우 alert창 뜨기
 			
-			url : url+"/admin/adminCateAdd",
-			
-			type: "post",
-			data : cateNameAndPublicYn,
-			dataType : "json",
-			success : function(cateVo){
-				console.log(cateVo);
-				render(cateVo);
-
-				for(var i=0; i<cateVo.length; i++){
-					render(cateVo[i]);
-				}
-			},
-			error : function(XHR, status, error) {
-				console.error(status + " : " + error);
+			if(result == true) {
+				alert("사용 가능합니다.")
+				
+			}else {
+				alert("중복된 타이틀입니다. 사용할 수 없습니다.")
+				return;
 			}
+			//중복되지 않았을 경우 리스트에 추가하기
+			$.ajax({
+				
+				url : url+"/admin/adminCateAdd",
+				
+				type: "post",
+				data : cateNameAndPublicYn,
+				dataType : "json",
+				success : function(cateVo){
+					console.log(cateVo);
+					render(cateVo);
+
+					for(var i=0; i<cateVo.length; i++){
+						render(cateVo[i]);
+					}
+				},
+				error : function(XHR, status, error) {
+					console.error(status + " : " + error);
+				}
+				
+			});
 			
-		});
-	
+			
+		},
+		error : function(XHR, status, error) {
+			console.error(status + " : " + error);
+		}
+		
+	});
 });
+
+
 
 function render(cateVo){
 	
@@ -134,6 +97,9 @@ function render(cateVo){
 	
 	$("tbody").prepend(str);
 }
+
+
+
 
 
 /*타이틀을 누르면 수정가능한 인풋박스 띄우기*/
@@ -232,4 +198,43 @@ $(".adminCate-submitBtn").on("click", ".adminCate-btnUpdate",function(){
 	});
 
 });
+
+
+/*카테고리 x버튼 누르면 삭제하기 메뉴가 있으면 삭제가 되면 안된다.. --삭제기능..*/
+$(".adminCate-delete").on("click", function(){
+	event.preventDefault();
+	console.log("클릭");
+	var cateDelete = $(this);
+	
+	console.log(cateDelete);
+	var categoryNo = cateDelete.parent().parent().data("no"); //data값 no를 가져온 이유는 최상위 값만 삭제하면 그 안에있던 데이터도 같이 삭제되기때문에(ex폴더)
+	console.log($('[data-no=' + categoryNo + ']'));
+	console.log(cateDelete.parent().parent());
+	console.log(categoryNo);
+	/*var categoryNo = $("#");*/
+	
+	$.ajax({
+		url : url+"/admin/adminCateDel",
+		type: "post",
+		data : {categoryNo : categoryNo},
+		dataType : "json",
+		success : function(cnt){
+			console.log(cnt);
+			/*성공시 처리해야 될 코드 작성 */
+			if(cnt==0){
+				console.log("실패");
+				alert('카테고리에 값이 남아있습니다.');
+			}
+			else{
+				console.log("넌 그냥 삭제");
+				$('[data-no=' + categoryNo + ']').remove();
+			}
+		},
+		error : function(XHR, status, error) {
+			console.error(status + " : " + error);
+		}	
+	});
+
+});
+
 
